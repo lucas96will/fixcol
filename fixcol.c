5cd9e043ecf952c7b22c1262a9fb1a935cea4f7b
lucas96will <lucas-w147@hotmail.com>
2022-04-18 23:20:07 -0300

    Primer fix a las pruebas

--- fixcol.c
+++ fixcol.c
@@ -6,7 +6,7 @@
 #define NOMBRE_ARCHIVO 2
 
 /* Funcion para verificar si dado un string contiene solo numeros
- * Post: */
+ * Post: devuelve 1 si es un digito, 0 en otro caso*/
 int solo_digitos(const char* s);
 
 
@@ -24,7 +24,7 @@ int fixcol(int columnas, FILE* entrada){
     FILE* archivo = entrada;
     /*Si la entrada es NULL entonces hay un error*/
     if(archivo == NULL){
-        fprintf(stderr, "Error: archivo fuente inaccesible.\n");
+        fprintf(stderr, "Error: archivo fuente inaccesible\n");
         return 2;
     }
 
@@ -35,7 +35,9 @@ int fixcol(int columnas, FILE* entrada){
         if(linea[strlen(linea)-1] == '\n'){
             linea[strlen(linea)-1] = '\0';
         }
-        printf("%s\n", linea);
+        if(strlen(linea) > 0 && linea[0] != '\n'){
+            printf("%s\n", linea);
+        }
     }
 
     fclose(archivo);
@@ -50,7 +52,7 @@ int fixcol(int columnas, FILE* entrada){
 int main(int argc, char* argv[]){
     if(argc < 2 || argc > 3){
         /*Valido que la cantidad de parametros sea la aceptada */
-        fprintf(stderr, "Error: Cantidad erronea de parametros.\n");
+        fprintf(stderr, "Error: Cantidad erronea de parametros\n");
         return 2;
     }
 
@@ -61,6 +63,10 @@ int main(int argc, char* argv[]){
     }
 
     int columnas = atoi(argv[CANT_BYTES]);
+
+    if(columnas <= 0){
+        return 2;
+    }
 
     if(argc == 2){
         return fixcol(columnas, stdin);
